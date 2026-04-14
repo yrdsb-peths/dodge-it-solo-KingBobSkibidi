c3518daec461b4e1006e7221f47d2f072890689e
KingBobSkibidi <lucianbai0808@gmail.com>
2026-04-14 11:19:12 -0400

    April 14: added first world and hero
    - made first world
    - added hero subclass, just moving forward

---
+++ Hero.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class hero here.
+ * 
+ * Lucian Bai
+ * April 14
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Hero(){
+        
+    }
+    
+    public void act()
+    {
+        move(10);
+    }
+}
